ec7fc462b7181c870d7ed12bca2587e36429cd87
Guillaume Bertin <pro.bertinguillaume@gmail.com>
2022-03-19 13:43:27 +0100

    feat: traumatologie / centre / equipe

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,24 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/centre', name: 'app_centre')]
+    public function centre(): Response
+    {
+        return $this->render('home/centre.html.twig');
+    }
+
+    #[Route('/equipe', name: 'app_equipe')]
+    public function equipe(): Response
+    {
+        return $this->render('home/equipe.html.twig');
+    }
+
+    #[Route('/traumatologie', name: 'app_traumatologie')]
+    public function traumatologie(): Response
+    {
+        return $this->render('home/traumatologie.html.twig');
+    }
+
+
 }
